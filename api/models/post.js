728199f9333c81c5f97321832751f6d549e09f52
jamesandere <jamesandere56@gmail.com>
2022-06-14 18:26:03 +0300

    Create post model

---
+++ api/models/post.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+    // userId: {type: String, required: true},
+    title: {type: String, required: true},
+    desc: {type: String, required: true},
+    body: {type: String, required: true},
+    image: {type: String},
+    categories: {type: Array}
+}, {timestamps: true});
+
+module.exports = mongoose.model('Post', postSchema);
