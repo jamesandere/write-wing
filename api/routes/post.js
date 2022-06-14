8a5dd875c30656918d6628eb0bf8135a744b2fa6
jamesandere <jamesandere56@gmail.com>
2022-06-14 18:56:57 +0300

    Create update route

--- api/routes/post.js
+++ api/routes/post.js
@@ -1,6 +1,19 @@
 const router = require('express').Router();
 const Post = require('../models/post');
 
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedPost = await Post.findByIdAndUpdate(
+            req.params.id, {
+                $set: req.body
+            }, {new: true}
+        );
+        res.status(200).send(updatedPost);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
 router.post('/', async (req, res) => {
     const {title, desc, body, image, categories} = req.body;
 
@@ -15,6 +28,15 @@ router.post('/', async (req, res) => {
 
         const savedPost = await newPost.save();
         res.status(200).send(savedPost);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).send(post);
     } catch (error) {
         res.status(500).send(error);
     }
