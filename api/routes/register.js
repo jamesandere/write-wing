ca3e8d397187bacb83688ef841bd7a110eb978ef
jamesandere <jamesandere56@gmail.com>
2022-06-17 14:41:53 +0300

    Create register route

---
+++ api/routes/register.js
@@ -0,0 +1,42 @@
+const Joi = require('joi');
+const bcrypt = require('bcrypt');
+const  User  = require('../models/user');
+const genAuthToken = require('../middleware/genAuthToken');
+const router = require('express').Router();
+
+router.post('/', async (req, res) => {
+    const schema = Joi.object({
+        firstName: Joi.string().min(3).max(30).required(),
+        lastName: Joi.string().min(3).max(30).required(),
+        email: Joi.string().min(3).max(200).required(),
+        password: Joi.string().min(5).max(200).required()
+    });
+
+    const { error } = schema.validate(req.body);
+
+    if(error) return res.status(400).send(error.details[0].message);
+    
+    let user = await User.findOne({ email: req.body.email});
+
+    if(user) return res.status(400).send("User already exists!");
+
+    user = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+
+    const salt = await bcrypt.genSalt(10);
+
+    user.password = await bcrypt.hash(user.password, salt);
+
+    user = await user.save();
+
+    const token = genAuthToken(user);
+
+    res.status(200).send(token);
+});
+
+module.exports = router;
